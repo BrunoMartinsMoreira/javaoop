d9185be0965b3c3d00699986bfcabc5c74493cfd
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-02-14 20:16:09 -0300

    feat:aula sobre strings

---
+++ src/aulas/Strings.java
@@ -0,0 +1,21 @@
+package aulas;
+
+public class Strings {
+  public static void main(String[] args) {
+    String s1,s2;
+    s1 = new String("nova string");
+    s2 = new String("nova string");
+
+    if(s1 == s2){//compara o mesmo endereço de memoria
+      System.out.println("Iguais");
+    }else {
+      System.out.println("Diferentes"); //vai cair aqui
+    }
+
+    if(s1.equals(s2)){//compara valor
+      System.out.println("Iguais com equals"); //vai cair aqui
+    }else {
+      System.out.println("Diferentes com equals");
+    }
+  }
+}

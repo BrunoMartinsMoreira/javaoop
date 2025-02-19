3f9000868a2d450abac82bd23d9c12ba5daf97bd
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-02-18 21:34:09 -0300

    feat: aula classes abstratas

--- src/br/com/moreira/javaoop/aulas/classesabstratas/PagamentoCartao.java
+++ src/br/com/moreira/javaoop/aulas/classesabstratas/PagamentoCartao.java
@@ -1,12 +1,16 @@
 package br.com.moreira.javaoop.aulas.classesabstratas;
 
 public class PagamentoCartao extends Pagamento {
-  public PagamentoCartao(String formaPagamento, int numeroParcelas, double valor) {
+  private final double taxaDeJuros;
+  public PagamentoCartao(String formaPagamento, int numeroParcelas, double valor, double taxaDeJuros) {
     super(formaPagamento, numeroParcelas, valor);
+    this.taxaDeJuros = taxaDeJuros;
   }
 
   @Override
   public void pagar(double total) {
-
+    double jurosMensais = (super.valor / super.numeroParcelas) * this.taxaDeJuros;
+    double parcelaComJuros = (super.valor / super.numeroParcelas) + jurosMensais;
+    setValor(parcelaComJuros * this.numeroParcelas);
   }
 }

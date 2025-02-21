77ada4bdb47c24fc0a42fe9a36e4d9f8381a5654
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-02-20 23:12:56 -0300

    feat: aula sobre try with resources

--- src/br/com/moreira/javaoop/aulas/excessoes/Main.java
+++ src/br/com/moreira/javaoop/aulas/excessoes/Main.java
@@ -3,6 +3,8 @@ package br.com.moreira.javaoop.aulas.excessoes;
 import br.com.moreira.javaoop.aulas.excessoes.customizadas.core.Produto;
 import br.com.moreira.javaoop.aulas.excessoes.customizadas.exceptions.ProdutoException;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     try{
@@ -20,6 +22,21 @@ public class Main {
     finally {
       // Igual no JS
       System.out.println("Sempre executa");
+    }
+
+    // try with resources
+    try( Scanner input = new Scanner(System.in)) {
+      int v1,v2,r;
+      System.out.println("Digite v1: ");
+      v1 = input.nextInt();
+
+      System.out.println("Digite v2: ");
+      v2 = input.nextInt();
+
+      r = v1/v2;
+      System.out.println("R: " + r);
+    }catch (Exception e) {
+      e.printStackTrace();
     }
   }
 }
